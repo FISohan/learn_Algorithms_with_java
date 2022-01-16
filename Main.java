9c47c135359de068817cbbbf9cd58775100a6bab
F. I. Sohan <66239209+FISohan@users.noreply.github.com>
2022-01-16 14:25:26 +0600

    quick-union Improvements
    Weighted quick union

--- Main.java
+++ Main.java
@@ -1,10 +1,14 @@
 import week_1.Union_find.QuickFindUF;
+import week_1.Union_find.QuickUnionUF;
 
 public class Main {
     public static void main(String[] args) {
-        QuickFindUF findUF = new QuickFindUF(10);
+        QuickUnionUF findUF = new QuickUnionUF(10);
         findUF.union(4, 3);
         findUF.union(3, 8);
+        findUF.union(6, 5);
+        findUF.union(9, 4);
+        findUF.union(6, 4);
         findUF.printResult();
     }
 }
--- week_1/Union_find/QuickUnionUF.java
+++ week_1/Union_find/QuickUnionUF.java
@@ -2,11 +2,14 @@ package week_1.Union_find;
 
 public class QuickUnionUF {
     private int[] id;
+    private int[] size;
 
     public QuickUnionUF(int N) {
+        size = new int[N];
         id = new int[N];
         for (int i = 0; i < N; i++) {
             id[i] = i;
+            size[i] = 1;
         }
     }
 
@@ -23,21 +26,38 @@ public class QuickUnionUF {
     }
 
     public void union(int p, int q) {
-        int rootOfQ = root(q);
-        int rootOfP = root(p);
+        int i = root(q);
+        int j = root(p);
+        if (i == j) {
+            return;
+        }
 
-        id[rootOfP] = rootOfQ;
+        if (size[i] < size[j]) {
+            id[i] = j;
+            size[j] += size[i];
+        }else{
+            id[j] = i;
+            size[i] += size[j];
+        }
     }
 
     public void printResult() {
-        String leftAlignFormat = "| %-15s | %-4d |%n";
+        String leftAlignFormat = "| %-15s | %-4d | %-4d |%n";
         System.out.format("+-----------------+------+%n");
-        System.out.format("| ID name     | INDEX   |%n");
+        System.out.format("| ID name     | INDEX   | size|%n");
         System.out.format("+-----------------+------+%n");
         for (int i = 0; i < id.length; i++) {
-            System.out.format(leftAlignFormat, "id:  " + id[i], i);
+            System.out.format(leftAlignFormat, "id:  " + id[i], i,size[i]);
         }
         System.out.format("+-----------------+------+%n");
 
     }
 }
+
+
+/*
+ 
+
+
+
+*/
